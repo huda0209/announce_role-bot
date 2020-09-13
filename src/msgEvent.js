b79e54247bd978f0e797282855c7415c3ab80e1e
huda0209 <huda030209@gmail.com>
2020-09-13 21:35:01 +0900

    [add] roleクラスとhelpクラスを追加

--- src/msgEvent.js
+++ src/msgEvent.js
@@ -7,6 +7,8 @@ main.js :MAIN  'MAIN CODE'　<= this
  -msgEvent.js :CLASS
  -panel.js  :CLASS
  -reactionEvent.js  :CLASS
+ -role.js  :CLASS
+ -help.js  :CLASS
  
 ran by node.js
 
@@ -14,8 +16,9 @@ ran by node.js
 
 */
 
-//classs
 const panel = require('./panel.js')
+const role = require('./role.js')
+const help = require('./help.js')
 
 const letter = [[":zero:","0⃣"],[":one:","1⃣"],[":two:","2⃣"],[":three:","3⃣"],[":four:","4⃣"],[":five:","5⃣"],[":six:","6⃣"],[":seven:","7⃣"],[":eight:","8⃣"],[":nine:","9⃣"],[":keycap_ten:","🔟"],[":regional_indicator_a:","🇦"],[":regional_indicator_b:","🇧"],[":regional_indicator_c:","🇨"],[":regional_indicator_d:","🇩"],[":regional_indicator_e:","🇪"],[":regional_indicator_f:","🇫"],[":regional_indicator_g:","🇬"],[":regional_indicator_h:","🇭"],[":regional_indicator_i:","🇮"],[":regional_indicator_j:","🇯"],[":regional_indicator_k:","🇰"],[":regional_indicator_l:","🇱"],[":regional_indicator_m:","🇲"],[":regional_indicator_n:","🇳"],[":regional_indicator_o:","🇴"],[":regional_indicator_p:","🇵"],[":regional_indicator_q:","🇶"],[":regional_indicator_r:","🇷"],[":regional_indicator_s:","🇸"],[":regional_indicator_t:","🇹"],[":regional_indicator_u:","🇺"],[":regional_indicator_v:","🇻"],[":regional_indicator_w:","🇼"],[":regional_indicator_x:","🇽"],[":regional_indicator_y:","🇾"],[":regional_indicator_z:","🇿"]]
 
@@ -33,6 +36,8 @@ class msgEvent {
         const json = this.json;
 
         const panele = new panel(client,json,roles)
+        const rolee = new role(client,json,roles)
+        const helpe = new help(client,json,roles)
 
         switch(command){
             case "panel" :
@@ -43,12 +48,12 @@ class msgEvent {
                 panele.panelCreate(message)
                 break;
 
-            case "owner" :
-
+            case "an" :
+                rolee.roleAdd([command, ...args],message)
                 break;
 
-            case "reloadpanel" :
-
+            case "help" :
+                helpe.help(message)
                 break;
           }
     }
