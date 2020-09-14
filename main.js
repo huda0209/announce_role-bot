f8c5c5b39330723b6e06dd7b1500c126aaed80ce
huda0209 <huda030209@gmail.com>
2020-09-14 23:44:23 +0900

    [add]admin機能を追加

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ main.js :MAIN  'MAIN CODE'　<= this
  
 ran by node.js
 
-2020-9-13
+2020-9-14
 
 */
 
@@ -21,57 +21,76 @@ const fs = require('fs');
 const discord = require("discord.js");
 
 //class
-const msgEvent = require('./src/msgEvent.js')
-const reactionEvent = require('./src/reactionEvent.js')
+const msgEvent = require('./src/msgEvent.js');
+const reactionEvent = require('./src/reactionEvent.js');
 
 //other 
 var json = JSON.parse(fs.readFileSync('./config/setting.json','utf8'));
 const roles = JSON.parse(fs.readFileSync('./config/roles.json','utf8'));
 const client = new discord.Client({restTimeOffset: 10});
-const letter = [[":zero:","0⃣"],[":one:","1⃣"],[":two:","2⃣"],[":three:","3⃣"],[":four:","4⃣"],[":five:","5⃣"],[":six:","6⃣"],[":seven:","7⃣"],[":eight:","8⃣"],[":nine:","9⃣"],[":keycap_ten:","🔟"],[":regional_indicator_a:","🇦"],[":regional_indicator_b:","🇧"],[":regional_indicator_c:","🇨"],[":regional_indicator_d:","🇩"],[":regional_indicator_e:","🇪"],[":regional_indicator_f:","🇫"],[":regional_indicator_g:","🇬"],[":regional_indicator_h:","🇭"],[":regional_indicator_i:","🇮"],[":regional_indicator_j:","🇯"],[":regional_indicator_k:","🇰"],[":regional_indicator_l:","🇱"],[":regional_indicator_m:","🇲"],[":regional_indicator_n:","🇳"],[":regional_indicator_o:","🇴"],[":regional_indicator_p:","🇵"],[":regional_indicator_q:","🇶"],[":regional_indicator_r:","🇷"],[":regional_indicator_s:","🇸"],[":regional_indicator_t:","🇹"],[":regional_indicator_u:","🇺"],[":regional_indicator_v:","🇻"],[":regional_indicator_w:","🇼"],[":regional_indicator_x:","🇽"],[":regional_indicator_y:","🇾"],[":regional_indicator_z:","🇿"]]
+const letter = [[":zero:","0⃣"],[":one:","1⃣"],[":two:","2⃣"],[":three:","3⃣"],[":four:","4⃣"],[":five:","5⃣"],[":six:","6⃣"],[":seven:","7⃣"],[":eight:","8⃣"],[":nine:","9⃣"],[":keycap_ten:","🔟"],[":regional_indicator_a:","🇦"],[":regional_indicator_b:","🇧"],[":regional_indicator_c:","🇨"],[":regional_indicator_d:","🇩"],[":regional_indicator_e:","🇪"],[":regional_indicator_f:","🇫"],[":regional_indicator_g:","🇬"],[":regional_indicator_h:","🇭"],[":regional_indicator_i:","🇮"],[":regional_indicator_j:","🇯"],[":regional_indicator_k:","🇰"],[":regional_indicator_l:","🇱"],[":regional_indicator_m:","🇲"],[":regional_indicator_n:","🇳"],[":regional_indicator_o:","🇴"],[":regional_indicator_p:","🇵"],[":regional_indicator_q:","🇶"],[":regional_indicator_r:","🇷"],[":regional_indicator_s:","🇸"],[":regional_indicator_t:","🇹"],[":regional_indicator_u:","🇺"],[":regional_indicator_v:","🇻"],[":regional_indicator_w:","🇼"],[":regional_indicator_x:","🇽"],[":regional_indicator_y:","🇾"],[":regional_indicator_z:","🇿"]];
+const prefix = json.bot.prefix;
 
 
 //start the bot
 client.on("ready", message => {
-  console.log(`bot is ready! ver: test \nlogin: ${client.user.tag}`);
-  client.user.setActivity('/helpでヘルプを表示 ver. 1.0.0', { type: 'PLAYING' })
+  console.log(`bot is ready! ver. ${json.bot.version} \nlogin: ${client.user.tag}`);
+  client.user.setActivity(`${prefix}helpでヘルプを表示 ver. ${json.bot.version}`, { type: 'PLAYING' });
 });
 
 //when bot join the guild, this event will load
 client.on("guildCreate", bot =>{
-    
   const GuildId = bot.id;
   const Owner = bot.ownerID;
-
   const adddata ={
               "GuildId" : GuildId,
               "Owner" : Owner,
-             }
+             };
   json.guild = (adddata)
   fs.writeFileSync('./config/setting.json', JSON.stringify(json, null, "\t"),'utf8');
-  console.log("guildCreate catch")
+  console.log("guildCreate catch");
   })
 
 //message event
 client.on("message", async message => {
-  const prefix = '/'
   if (message.content.startsWith(prefix)){
-    const [command, ...args] = message.content.slice(prefix.length).split(' ')
+    const [command, ...args] = message.content.slice(prefix.length).split(' ');
     if(command === "stop"){
-      if(message.author.id === message.guild.ownerID){
-        console.log(`server was stoped by${message.author.tag}`);
-        await message.delete()
-        process.exit(0);}
+      if(message.author.id === message.guild.ownerID || json.guild.Admin.indexOf(message.author.id)>-1){
+        console.log(`server was stoped by ${message.author.tag}`);
+        await message.delete();
+        client.destroy();
+        process.exit(0);};
     };
-    const msge = new msgEvent(client,message,json)
-    msge.msgEvent([command, ...args],roles)
+    const msge = new msgEvent(client,message,json);
+    msge.msgEvent([command, ...args],roles);
+    
+    if(command.toLowerCase() === "admin" &&(message.author.id === message.guild.ownerID || json.guild.Admin.indexOf(message.author.id)>-1)){
+      switch(args[0].toLowerCase()){
+          case "add" :
+              json.guild.Admin.push(message.mentions.members.first().id);
+              fs.writeFileSync('./config/setting.json', JSON.stringify(json, null, "\t"),'utf8');
+              console.log(`Add admin ${message.mentions.members.first()}`);
+              break;
+
+          case "remove" :
+              delete json.guild.Admin[json.guild.Admin.indexOf(message.mentions.members.first().id)];
+              fs.writeFileSync('./config/setting.json', JSON.stringify(json, null, "\t"),'utf8');
+              console.log(`Remove admin ${message.mentions.members.first()}`);
+              break;
+
+          default :
+              message.channel.send(`コマンドが違います。`);
+              break;
+      };
+  };
   };
 })
 
 client.on("messageReactionAdd", async(messageReaction ,user) =>{
   if(user.bot) return;
-  const reactionEvente = new reactionEvent(client,json,roles)
-  reactionEvente.addrole(messageReaction ,user)
+  const reactionEvente = new reactionEvent(client,json,roles);
+  reactionEvente.addrole(messageReaction ,user);
 })    
 
 
@@ -93,8 +112,8 @@ if(process.argv.length>=3){
       token = json.bot.TEST_TOKEN;
       break;
     default :
-      console.log(`\nUnknown command. \nUsage \n node main.js main : use main token \n node main.js test : use test token`)
+      console.log(`\nUnknown command. \nUsage \n node main.js main : use main token \n node main.js test : use test token`);
       process.exit(0);
   };
-}else token = json.bot.MAIN_TOKEN
+}else token = json.bot.MAIN_TOKEN;
 client.login(token);
