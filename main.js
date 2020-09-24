5ba407c2ad7b3a9b681f4506e3f8d163a3dcbb4e
huda0209 <huda030209@gmail.com>
2020-09-24 23:16:04 +0900

    [add]Privileged Intentsを設定

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ main.js :MAIN  'MAIN CODE'　<= this
  
 ran by node.js
 
-2020-9-20
+2020-9-24
 
 */
 
@@ -25,10 +25,11 @@ const msgEvent = require('./src/msgEvent.js');
 const reactionEvent = require('./src/reactionEvent.js');
 
 //other 
+const option = {ws: {intents: discord.Intents.ALL}, restTimeOffset: 10};
 let json = JSON.parse(fs.readFileSync('./config/setting.json','utf8'));
 const roles = JSON.parse(fs.readFileSync('./config/roles.json','utf8'));
 const package = require('./package.json')
-const client = new discord.Client({restTimeOffset: 10});
+const client = new discord.Client(option);
 const letter = [[":zero:","0⃣"],[":one:","1⃣"],[":two:","2⃣"],[":three:","3⃣"],[":four:","4⃣"],[":five:","5⃣"],[":six:","6⃣"],[":seven:","7⃣"],[":eight:","8⃣"],[":nine:","9⃣"],[":keycap_ten:","🔟"],[":regional_indicator_a:","🇦"],[":regional_indicator_b:","🇧"],[":regional_indicator_c:","🇨"],[":regional_indicator_d:","🇩"],[":regional_indicator_e:","🇪"],[":regional_indicator_f:","🇫"],[":regional_indicator_g:","🇬"],[":regional_indicator_h:","🇭"],[":regional_indicator_i:","🇮"],[":regional_indicator_j:","🇯"],[":regional_indicator_k:","🇰"],[":regional_indicator_l:","🇱"],[":regional_indicator_m:","🇲"],[":regional_indicator_n:","🇳"],[":regional_indicator_o:","🇴"],[":regional_indicator_p:","🇵"],[":regional_indicator_q:","🇶"],[":regional_indicator_r:","🇷"],[":regional_indicator_s:","🇸"],[":regional_indicator_t:","🇹"],[":regional_indicator_u:","🇺"],[":regional_indicator_v:","🇻"],[":regional_indicator_w:","🇼"],[":regional_indicator_x:","🇽"],[":regional_indicator_y:","🇾"],[":regional_indicator_z:","🇿"]];
 const prefix = json.bot.prefix;
 
