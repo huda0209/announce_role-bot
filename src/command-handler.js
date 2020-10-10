4ec78884fe84f1da5425dd59c45165eec7a5e6d7
huda0209 <huda030209@gmail.com>
2020-10-10 21:51:53 +0900

    [change/delete]クラスからモジュールに変更/インスタンス化する行を削除

--- src/command-handler.js
+++ src/command-handler.js
@@ -4,12 +4,12 @@ created by huda0209
 announce role bot for discord bot 
 
 main.js :MAIN  'MAIN CODE'
- -msgEvent.js :CLASS　<= this
- -panel.js  :CLASS
- -reactionEvent.js  :CLASS
- -role.js  :CLASS
- -help.js  :CLASS
- -admin.js  :CLASS
+ -command-handler.js :module　<= this
+ -panel.js  :module
+ -reactionEvent.js  :module
+ -announceRole.js  :module
+ -help.js  :module
+ -admin.js  :module
  
 ran by node.js
 
@@ -17,48 +17,33 @@ ran by node.js
 
 */
 
-const panel = require('./panel.js');
-const role = require('./role.js');
-const help = require('./help.js');
-const admin = require('./admin.js')
+const panel = require('./command/panel.js');
+const announceRole = require('./command/announceRole.js');
+const help = require('./command/help.js');
+const admin = require('./command/admin.js')
 
 
-class msgEvent {
-    constructor(client) {
-        this.client = client;
-    }
 
-    async msgEvent ([command, ...args],message,guildData,BOT_DATA){
-        const client = this.client;
+const commandHandler = async function ([command, ...args],message,guildData,BOT_DATA,client){
 
-        const panele = new panel();
-        const rolee = new role(client);
-        const helpe = new help();
-        const admine = new admin(client)
-
-        switch(command.toLowerCase()){
-            case "panel" :
-                panele.panelCreate(message,guildData);
-                break;
-
-            case "pa" :
-                panele.panelCreate(message,guildData);
-                break;
-
-            case "an" :
-                rolee.roleAdd([command, ...args],message,guildData,BOT_DATA);
-                break;
-
-            case "admin" :
-                if(!(message.author.id === message.guild.ownerID || guildData.guild.Admin.indexOf(message.author.id)>-1)) break;
-                admine.admin([command, ...args],message,guildData)
-                break;
-
-            case "help" :
-                helpe.help(message,guildData,BOT_DATA);
-                break;
-          }
-    }
+    switch(command.toLowerCase()){
+        case "panel" :
+            panel.panelCreate(message,guildData);
+            break;
+        case "pa" :
+            panel.panelCreate(message,guildData);
+            break;
+        case "an" :
+            announceRole.announceRole_Manager([command, ...args],message,guildData,BOT_DATA);
+            break;
+        case "help" :
+            help.help(message,guildData,BOT_DATA);
+            break;
+        case "admin" :
+            if(!(message.author.id === message.guild.ownerID || guildData.guild.Admin.indexOf(message.author.id)>-1)) break;
+            admin.adminManager([command, ...args],message,guildData,client)
+            break;
+      }
 }
 
-module.exports = msgEvent
+exports.commandHandler = commandHandler
