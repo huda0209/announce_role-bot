084674da2441b5228a9178236d9715b967adcff2
huda0209 <huda030209@gmail.com>
2020-10-24 21:35:04 +0900

    [change]最後のロールの後ろにコンマを入れないように変更

--- src/command/help.js
+++ src/command/help.js
@@ -13,15 +13,15 @@ main.js :MAIN  'MAIN CODE'
  
 ran by node.js
 
-2020-10-10
+2020-10-24
 
 */
 
 const help = async function(message,GuildData,BOT_DATA){
     let content = `**${GuildData.content.function_name} 使い方**\n\`${BOT_DATA.PREFIX}panel (省略形 ${BOT_DATA.PREFIX}pa) \`で通知のオンオフを設定できるパネルを生成します。\nリアクションすることでオンオフできます。\n\`${BOT_DATA.PREFIX}an <${GuildData.content.command_alias}名> on/off \`でもオンオフが設定できます。\n設定できる${GuildData.content.command_alias} 一覧\n\`\`\``
     for(let i=0;i<GuildData.roles.length;i++){
-        content = `${content}${GuildData.roles[i][0]}, `;
-    }
+        content = `${content}${GuildData.roles[i][0]}${i<(GuildData.roles.length -1) ? ",":""} `;
+    };
     content = `${content}\`\`\``;
     message.channel.send(content);
 };
