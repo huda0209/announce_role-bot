c62aae980e8c976480da6c45da620390689e1eab
huda0209 <huda030209@gmail.com>
2020-10-24 21:21:17 +0900

    [add]admin リスト機能追加

--- src/command/admin.js
+++ src/command/admin.js
@@ -13,7 +13,7 @@ main.js :MAIN  'MAIN CODE'
  
 ran by node.js
 
-2020-10-10
+2020-10-24
 
 */
 
@@ -22,9 +22,9 @@ const logger = require('../util/logger.js')
 
 
 const adminManager = async function ([command, ...args],message,guildData,client){
-    if(args.length<2) return message.channel.send(`コマンドの引数が足りません。`);
     switch(args[0].toLowerCase()){
         case "add" :
+            if(args.length<2) return message.channel.send(`コマンドの引数が足りません。`);
             if(guildData.guild.Admin.indexOf(message.mentions.members.first().id)>=0) return message.channel.send(`そのユーザーは追加済みです`);
             guildData.guild.Admin.push(message.mentions.members.first().id);
             fs.writeFileSync('./config/guild/guild.json', JSON.stringify(guildData, null, "\t"),'utf8');
@@ -33,12 +33,25 @@ const adminManager = async function ([command, ...args],message,guildData,client
             break;
 
         case "remove" :
+            if(args.length<2) return message.channel.send(`コマンドの引数が足りません。`);
             if(guildData.guild.Admin.indexOf(message.mentions.members.first().id)==-1) return message.channel.send(`そのユーザーはリストに入っていません`);
             delete guildData.guild.Admin[guildData.guild.Admin.indexOf(message.mentions.members.first().id)];
             guildData.guild.Admin = guildData.guild.Admin.filter(Boolean);
             fs.writeFileSync('./config/guild/guild.json', JSON.stringify(guildData, null, "\t"),'utf8');
             logger.info(`Remove admin {red}${message.mentions.members.first().user.tag}`);
             message.channel.send(`ユーザーを削除しました`);
+            break;
+
+        case "list" :
+            let adminList = `**${message.guild.name} Adminリスト**\nOwner : *${(await client.users.fetch(message.guild.ownerID)).tag}*\nAdmin :`;
+            if(guildData.guild.Admin.length<1){
+                adminList = `${adminList} なし`;
+            }
+            console.log(client.users.fetch(guildData.guild.Admin[0]))
+            for(let i=0;i<guildData.guild.Admin.length;i++){
+                adminList = `${adminList}\n*${(await client.users.fetch(guildData.guild.Admin[i])).tag}*`;
+            };
+            message.channel.send(adminList)
             break;
 
         default :
