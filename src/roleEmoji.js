f69e1addebf234e39e81716514afa833996b0de9
huda0209 <65341393+huda0209@users.noreply.github.com>
2020-12-24 23:42:58 +0900

    [change]引数をmessageからguildに変更

--- src/roleEmoji.js
+++ src/roleEmoji.js
@@ -22,7 +22,7 @@ ran by node.js
 const letter = [[":zero:","0⃣"],[":one:","1⃣"],[":two:","2⃣"],[":three:","3⃣"],[":four:","4⃣"],[":five:","5⃣"],[":six:","6⃣"],[":seven:","7⃣"],[":eight:","8⃣"],[":nine:","9⃣"],[":keycap_ten:","🔟"],[":regional_indicator_a:","🇦"],[":regional_indicator_b:","🇧"],[":regional_indicator_c:","🇨"],[":regional_indicator_d:","🇩"],[":regional_indicator_e:","🇪"],[":regional_indicator_f:","🇫"],[":regional_indicator_g:","🇬"],[":regional_indicator_h:","🇭"],[":regional_indicator_i:","🇮"],[":regional_indicator_j:","🇯"],[":regional_indicator_k:","🇰"],[":regional_indicator_l:","🇱"],[":regional_indicator_m:","🇲"],[":regional_indicator_n:","🇳"],[":regional_indicator_o:","🇴"],[":regional_indicator_p:","🇵"],[":regional_indicator_q:","🇶"],[":regional_indicator_r:","🇷"],[":regional_indicator_s:","🇸"],[":regional_indicator_t:","🇹"],[":regional_indicator_u:","🇺"],[":regional_indicator_v:","🇻"],[":regional_indicator_w:","🇼"],[":regional_indicator_x:","🇽"],[":regional_indicator_y:","🇾"],[":regional_indicator_z:","🇿"]]
 
 
-exports.roleEmojiSet = function roleEmojiSet(message, GuildData){
+exports.roleEmojiSet = function roleEmojiSet(guild, GuildData){
 
     let roleList = GuildData.roles;
     let roleIndex = 0;
@@ -32,7 +32,7 @@ exports.roleEmojiSet = function roleEmojiSet(message, GuildData){
             roleIndex++;
             continue;
         }else{
-            let customEmoji = message.guild.emojis.cache.get(roleList[i][2])
+            let customEmoji = guild.emojis.cache.get(roleList[i][2])
             roleList[i].push([`<:${customEmoji.name}:${customEmoji.id}>`,customEmoji]);
         }
     }
